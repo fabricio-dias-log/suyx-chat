1bf9157c82447579e7c1507867d85a2dc7ac9ed4
Fabricio Dias <fabricio270909hotmail.com>
2024-02-02 14:46:42 -0300

    Chat is complete

--- servidor.js
+++ servidor.js
@@ -9,9 +9,13 @@ const io = require('socket.io')(httpServer);
 
 app.use(express.static('./public'))
 httpServer.listen(port)
+// httpServer.listen(port, (ipv4)) - para configurar na rede e poder ser usado por usuários da rede
 
-io.addListener('connection', () => {
+io.addListener('connection', (socket) => {
     console.log('connected');
+    socket.addListener('Nova mensagem', (msg) => {
+        io.emit('Nova mensagem', msg);
+    })
 })
 
 // app.get('/', (req, res) => res.send('Hello World!'))
